70f9a583065c6e86e9965375f41e60837a7cbabf
Thel-V <arbiter.arbiter.pc@gmail.com>
2025-07-03 08:28:15 +0200

    New Face

---
+++ src/main/java/dev/thel/service/WebConfig.java
@@ -0,0 +1,28 @@
+package dev.thel.service;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp"); // --> .../home | NOT --> .../home.jsp
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+
+        registry.addResourceHandler("/pictures/**")
+                .addResourceLocations("classpath:/static/pictures/");
+
+    }   // Der Kürzer, /css/... = rescources/static/css....
+}
+
